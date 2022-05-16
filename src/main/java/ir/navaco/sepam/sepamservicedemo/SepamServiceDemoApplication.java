36dd07abb4d631dd5a115af6b46026b79b26e84d
r.mohammadi <reza.mohamadi1001@gmail.com>
2022-05-16 11:54:27 +0430

    rffgt

---
+++ src/main/java/ir/navaco/sepam/sepamservicedemo/SepamServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package ir.navaco.sepam.sepamservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepamServiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SepamServiceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/navaco/sepam/sepamservicedemo/SepamServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.navaco.sepam.sepamservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepamServiceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
